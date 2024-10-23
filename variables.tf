5d767be846d61d5b32b37ee359cf11284ae9b9c0
trungkien15work <trungkien15work@gmail.com>
2024-10-23 14:47:47 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "access_key" {
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key" {
-  type        = string
-  sensitive   = true
-}
-
 variable "region" {
   type        = string
   default     = "us-east-2"
